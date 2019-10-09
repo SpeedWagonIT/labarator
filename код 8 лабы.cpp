bf7edc039a129b28baa3f2937ff0a3a7d9982caa
SpeedWagonIT <47567188+SpeedWagonIT@users.noreply.github.com>
2019-10-09 21:01:29 +0300

    09.10.19
    Добавлена возможность вводить недопустимые символы

--- код 8 лабы.cpp
+++ код 8 лабы.cpp
@@ -24,38 +24,32 @@ void process_line(char line[])
 	char c; // текущий символ 
 	int word = NO; // признак слова 
 	int symb = NO; // индикатор наличия лишних символов 
-	char *in_ptr = line; // указатель на позицию текущего символа исходной cтроки 
-	char *out_ptr = line; // указатель на позицию текущего символа результирующей строки 
-	char *word_ptr = line; // указатель на позицию начала слова 
-	char *pred_word_ptr = line; // указатель на позицию начала предыдущего слова 
-	
+	char* in_ptr = line; // указатель на позицию текущего символа исходной cтроки 
+	char* out_ptr = line; // указатель на позицию текущего символа результирующей строки 
+	char* word_ptr = line; // указатель на позицию начала слова 
+	char* pred_word_ptr = line; // указатель на позицию начала предыдущего слова 
+
 
 	do
 	{
 		c = *in_ptr; // взять текущий символ из буфера 
-		if (c == ' ' || c == '\n' ) // разделитель найден
+		if (c == '\0')
 		{
-			if (symb == NO)
+			if (pred_word_ptr != word_ptr)
+				for (; *(pred_word_ptr - 1) != '\0'; word_ptr++, pred_word_ptr++)
+				{
+					*pred_word_ptr = *word_ptr;
+				}
+			break;
+		}
+		else if (c == ' ') // разделитель найден
+		{
+			if (word == YES && symb == NO)
 			{
 				pred_word_ptr = word_ptr;
 			}
-			if (word == YES || symb == YES)
-			{
-				for (; word_ptr < in_ptr; word_ptr++, out_ptr++)
-				{
-				}
-			}
-			*out_ptr++ = c;
 			symb = NO;
 			word = NO;
-		}
-		else if (c == '\0') // если то конец строки, то последнее сорвр пишем вместо предпоследнего
-		{
-			for (; word_ptr < in_ptr; out_ptr++)
-			{
-				*pred_word_ptr++ = *word_ptr++;
-			}
-			*pred_word_ptr = c;
 		}
 		else
 		{
@@ -71,12 +65,5 @@ void process_line(char line[])
 		}
 		in_ptr++;
 	} while (c != '\0');// продолжать до конца строки
-	*out_ptr = '\0';//устанавливаем конец строки
 }
-
-
-
-
-			
-					
 
