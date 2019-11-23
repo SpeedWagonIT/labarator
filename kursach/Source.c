03a7dd46be433b78c32b463944aee383eeb94064
SpeedWagonIT <47567188+SpeedWagonIT@users.noreply.github.com>
2019-11-23 21:52:56 +0300

    Add files via upload

--- kursach/Source.c
+++ kursach/Source.c
@@ -13,19 +13,19 @@ typedef struct {
 	char holiday[100];
 }almс;
 
-void out(FILE *almanac, almс *mp, int size)
+void out(FILE *f, almс *mp, int size)
 {
-	fwrite(&size, sizeof(int), 1, almanac);
-	fwrite(mp, sizeof(almс), size, almanac);
+	fwrite(&size, sizeof(int), 1, f);
+	fwrite(mp, sizeof(almс), size, f);
 }
 
-void in(FILE *almanac, almс *mp, int *size)
+void in(FILE *f, almс *mp, int *size)
 {
-	fread(size, sizeof(int), 1, almanac);
-	fread(mp, sizeof(almс), *size, almanac);
+	fread(size, sizeof(int), 1, f);
+	fread(mp, sizeof(almс), *size, f);
 }
 
-almс red()
+almс add()
 {
 	almс mp;
 	printf("\n select month: ");
@@ -55,15 +55,65 @@ void show(almс *mp, int size)
 	}
 	
 }
-
+//
+//int day(const void *f, const void *s)
+//{
+//	return strcmp(((almс*)f)->day, ((almс*)s)->day) > 0;
+//}
+//
+//int month(const void *f, const void *s)
+//{
+//	return (((almс*)f)->month - ((almс*)s)->month < 0);
+//}
 
 
 int main(void)
 {
 	almс mp[100];
+	int size = 0, i;
+	FILE *f = fopen("f.txt", "rb");
+	if (f)
+	{
+		in(f, mp, &size);
+		fclose(f);
+	}
+	f = fopen("f.txt", "wb");
+	while (1)
+	{
+		printf("\n1 add\n2 edit \n3 delete \n4 sort(not yet) \n5 show all \n6 find(not yet) \n0 exit\n");
+		scanf("%d", &i);
+		switch (i)
+		{
+		case 1:
+			mp[size] = add();
+			size++;
+			break;
 
+		case 2:
+			printf("\ni: ");
+			scanf("%d", &i);
+			mp[i] = add();
+			break;
 
+		case 3:
+			printf("\ni: ");
+			scanf("%d", &i);
+			--size;
+			for (; i < size; i++)
+				mp[i] = mp[i + 1];
+			break;
 
+		case 5:
+			show(mp, size);
+			break;
 
+		case 0:
+			out(f, mp, size);
+			fclose(f);
+			return 0;
+		}
 
+	}
 }
+
+
